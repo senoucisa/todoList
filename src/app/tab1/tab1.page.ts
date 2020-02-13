99e01adbb072675952144f82ed86c006f80d745c
samia <samia.ouali@lepoles.com>
2020-02-13 16:22:39 +0100

    exercice appli

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+todos = [
+  { done: false, text: 'finish Svelte tutorial' },
+  { done: false, text: 'build an app' },
+  { done: false, text: 'world domination' }
+];
+ add() {
+  this.todos = this.todos.concat({ done: false, text: '' });
+}
+ clear() {
+  this.todos= this.todos.filter(t => !t.done);
 
+}
   constructor() {}
-
 }
